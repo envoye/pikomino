86673e4c5e4a43a6e155bab028f58e4e037c7946
paulocarvalho73 <paulocarvalho73@gmail.com>
2012-12-20 23:07:44 +0000

    Alteracoes com o Netbean para testar incluindo + 1 Class

--- Pikomino/src/pikomino/Main.java
+++ Pikomino/src/pikomino/Main.java
@@ -2,13 +2,15 @@ package pikomino;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String teste3 = "Success!";
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String teste3 = "Success!";
         System.out.println(teste3);
-	}
-
+        PecasJogo pc = new PecasJogo();
+        System.out.println("O valor é " + pc.inser(10));
+        
+    }
 }
---
+++ Pikomino/src/pikomino/PecasJogo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pikomino;
+
+/**
+ *
+ * @author Paulo Carvalho
+ */
+public class PecasJogo {
+    
+    public int inser(int n){
+        int p = n+20;
+        return p;
+    }
+    
+}
