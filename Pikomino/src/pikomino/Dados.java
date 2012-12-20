a7f83ea4edd1a22727eeecbe1451d3c5a406fbda
paulocarvalho73@gmail.com <paulocarvalho73@gmail.com@74083239-f454-ac18-d78f-1a55699b34c1>
2012-12-20 21:48:28 +0000

    Alteracoes com o Netbean para testar

---
+++ Pikomino/src/pikomino/Dados.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pikomino;
+
+/**
+ *
+ * @author Paulo Carvalho
+ */
+public class Dados {
+    public int nlancamento(int n){
+        n+=10;
+        return n;
+    }
+    
+}
---
+++ Pikomino/src/pikomino/Pikomino.java
@@ -0,0 +1,24 @@
+/**
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//para testar
+package pikomino;
+
+/**
+ *
+ * @author Paulo Carvalho
+ */
+public class Pikomino {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Dados d = new Dados();
+        System.out.println("O Novo valor é " + d.nlancamento(2) );
+        
+        
+        // TODO code application logic here
+    }
+}
