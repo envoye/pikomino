d0a4552c1944cad4fbe1e465660df0d8e6be34d8
diogo.parreira.23 <diogo.parreira.23@gmail.com>
2012-12-22 16:49:14 +0000

    View

---
+++ Pikomino/src/pikomino/view/GamePanel.java
@@ -0,0 +1,88 @@
+package pikomino.view;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Transparency;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+import pikomino.model.Model;
+import pikomino.view.screen.Screen;
+
+public class GamePanel extends JPanel {
+
+	
+	static public int widthScreen = 800;
+	static public int heightScreen = 600;
+	
+	private int posX, posY, widthImage, heightImage;
+	
+	
+	private Model model = null;
+	
+	Screen screen;
+	
+	public GamePanel(Model model)
+	{
+		this.model = model;
+		screen = new Screen(this, model, widthScreen, heightScreen);
+		
+	}
+	
+	@Override
+    public synchronized void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice gs = ge.getDefaultScreenDevice();
+        GraphicsConfiguration gc = gs.getDefaultConfiguration();
+        
+        BufferedImage bimage = gc.createCompatibleImage(widthScreen, heightScreen, Transparency.OPAQUE);
+        Graphics2D gBuffered = bimage.createGraphics();
+        
+        g.setColor(Color.white);
+        g.fillRect(0,0,getWidth(),getHeight());
+        
+//        gBuffered.setColor(Color.red);
+//        gBuffered.fillRect(0,0,getWidth(),getHeight());
+        
+        screen.render(gBuffered);
+
+        if(getWidth()>getHeight()*1.33333f)
+        {
+        	widthImage = Math.round(getHeight() + getHeight()*(1f/3f));
+        	heightImage = getHeight();
+        	
+        	posX = Math.round(Math.max(0, (getWidth() - widthImage)/2f));
+        	posY = 0;
+        	
+        }
+        else
+        {
+        	if(getWidth()<getHeight()*1.33333f)
+        	{
+        		widthImage = getWidth();
+            	heightImage = Math.round(getWidth() - getWidth()*(1f/4f));
+            	
+        		posX = 0;
+            	posY = Math.round(Math.max(0, (getHeight() - heightImage)/2f));
+            	
+        	}
+        	else
+        	{
+	        	posX = 0;
+	        	posY = 0;
+	        	widthImage = getWidth();
+	        	heightImage = getHeight();
+        	}
+        }
+        
+        
+        g.drawImage(bimage, posX, posY, widthImage,heightImage, this);
+    }
+}
---
+++ Pikomino/src/pikomino/view/PikominoFrame.java
@@ -0,0 +1,34 @@
+package pikomino.view;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import pikomino.model.Model;
+
+public class PikominoFrame extends JFrame {
+
+	private Model model;
+	private JPanel contentPane;
+	private GamePanel gamePanel;
+	
+	public PikominoFrame(Model model) {
+		
+		this.model = model;
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800+26, 600+71);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
+		contentPane.add(new GamePanel(model), BorderLayout.CENTER);
+	}
+	
+	
+	
+}
