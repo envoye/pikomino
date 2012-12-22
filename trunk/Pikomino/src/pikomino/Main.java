d0e28d886e0c5cb705673dace37020527bc5e334
kevincadima@gmail.com <kevincadima@gmail.com@74083239-f454-ac18-d78f-1a55699b34c1>
2012-12-22 11:32:30 +0000

    teste

--- trunk/Pikomino/src/pikomino/Main.java
+++ trunk/Pikomino/src/pikomino/Main.java
@@ -7,7 +7,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        String teste3 = "Success!";
+        String teste3 = "Success2!";
         System.out.println(teste3);
     }
 }
